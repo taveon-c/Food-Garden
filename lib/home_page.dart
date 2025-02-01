999cbfcf16680d0193df70dbb223452f0daf9761
GangstaFroggy <TaveonFrog09@gmail.com>
2025-02-01 12:50:28 -0500

    Got rid of ads

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,12 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application/consent_manager.dart';
 import 'package:flutter_application/database.dart';
 import 'package:flutter_application/plant.dart';
 import 'package:flutter_application/plant_home_widget.dart';
 import 'package:flutter_application/plant_search_delegate.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,15 +14,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final _consentManager = ConsentManager();
-  var _isMobileAdsInitializeCalled = false;
-  // ignore: unused_field
-  var _isPrivacyOptionsRequired = false;
-  BannerAd? _bannerAd;
-  bool _isLoaded = false;
-
-  final String _adUnitId = 'ca-app-pub-3940256099942544/9214589741';
-
   final SqlDb _sqldb = SqlDb();
   final SearchController controller = SearchController();
   late Timer _timer;
@@ -37,21 +26,6 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-
-    _consentManager.gatherConsent((consentGatheringError) {
-      if (consentGatheringError != null) {
-        // Consent not obtained in current session.
-        debugPrint(
-            "${consentGatheringError.errorCode}: ${consentGatheringError.message}");
-      }
-      // Check if a privacy options entry point is required.
-      _getIsPrivacyOptionsRequired();
-      // Attempt to initialize the Mobile Ads SDK.
-      _initializeMobileAdsSDK();
-    });
-    // This sample attempts to load ads using consent obtained in the previous session.
-    _initializeMobileAdsSDK();
-
     _timer = Timer.periodic(Duration(seconds: 1), (timer) {
       setState(() {});
     });
@@ -59,7 +33,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void dispose() {
-    _bannerAd?.dispose();
     _timer.cancel();
     super.dispose();
   }
@@ -126,85 +99,9 @@ class _HomePageState extends State<HomePage> {
                 future: readData(),
               ),
             ),
-            if (_bannerAd != null && _isLoaded)
-              SafeArea(
-                child: SizedBox(
-                  width: _bannerAd!.size.width.toDouble(),
-                  height: _bannerAd!.size.height.toDouble(),
-                  child: AdWidget(ad: _bannerAd!),
-                ),
-              ),
           ],
         ),
       ),
     );
-  }
-
-  void _loadAd() async {
-    // Only load an ad if the Mobile Ads SDK has gathered consent aligned with
-    // the app's configured messages.
-    var canRequestAds = await _consentManager.canRequestAds();
-    if (!canRequestAds) {
-      return;
-    }
-    if (!mounted) {
-      return;
-    }
-    // Get an AnchoredAdaptiveBannerAdSize before loading the ad.
-    final size = await AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(
-        MediaQuery.sizeOf(context).width.truncate());
-
-    if (size == null) {
-      // Unable to get width of anchored banner.
-      return;
-    }
-
-    BannerAd(
-      adUnitId: _adUnitId,
-      request: const AdRequest(),
-      size: size,
-      listener: BannerAdListener(
-        // Called when an ad is successfully received.
-        onAdLoaded: (ad) {
-          setState(() {
-            _bannerAd = ad as BannerAd;
-            _isLoaded = true;
-          });
-        },
-        // Called when an ad request failed.
-        onAdFailedToLoad: (ad, err) {
-          ad.dispose();
-        },
-        onAdOpened: (Ad ad) {},
-        onAdClosed: (Ad ad) {},
-        onAdImpression: (Ad ad) {},
-      ),
-    ).load();
-  }
-
-  /// Redraw the app bar actions if a privacy options entry point is required.
-  void _getIsPrivacyOptionsRequired() async {
-    if (await _consentManager.isPrivacyOptionsRequired()) {
-      setState(() {
-        _isPrivacyOptionsRequired = true;
-      });
-    }
-  }
-  /// Initialize the Mobile Ads SDK if the SDK has gathered consent aligned with
-  /// the app's configured messages.
-  void _initializeMobileAdsSDK() async {
-    if (_isMobileAdsInitializeCalled) {
-      return;
-    }
-
-    if (await _consentManager.canRequestAds()) {
-      _isMobileAdsInitializeCalled = true;
-
-      // Initialize the Mobile Ads SDK.
-      MobileAds.instance.initialize();
-
-      // Load an ad.
-      _loadAd();
-    }
   }
 }
--- lib/plant.dart
+++ lib/plant.dart
@@ -11,13 +11,13 @@ class Plant {
   Plant(this.name, this.date, this.id);
 
   double getProgress(){
-    var timeElapsed = DateTime.now().difference(DateTime.parse(date)).inSeconds;
+    var timeElapsed = DateTime.now().difference(DateTime.parse(date)).inDays;
     var growthTime = plantVarieties[name]?['harvest'];
     return min(timeElapsed/growthTime, 1.0);
   }
 
   int getDaysLeft() {
-    var timeElapsed = DateTime.now().difference(DateTime.parse(date)).inSeconds;
+    var timeElapsed = DateTime.now().difference(DateTime.parse(date)).inDays;
     var growthTime = plantVarieties[name]?['harvest'];
     return max(growthTime - timeElapsed, 0);
   }
